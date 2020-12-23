73a5263ae0498e427e553c1ce7ac25f114c74396
Ian Sampson <ian_sampson@brown.edu>
2020-12-23 13:43:19 -0800

    Expose public initializers for AppAttest structs

--- Sources/AppAttest/API.swift
+++ Sources/AppAttest/API.swift
@@ -14,25 +14,21 @@ public enum AppAttest { }
 // MARK: - Attestation
 
 extension AppAttest {
+    // Client response
+    public struct AttestationResponse: Codable {
+        let attestation: Data
+        let keyID: Data
+        
+        public init(attestation: Data, keyID: Data) {
+            self.attestation = attestation
+            self.keyID = keyID
+        }
+    }
+    
+    // Server result (to be stored in database)
     public struct AttestationResult {
         let publicKey: P256.Signing.PublicKey
         let receipt: Data
-    }
-    
-    public struct AppID {
-        let teamID: String
-        let bundleID: String
-        
-        var description: String {
-            "\(teamID).\(bundleID)"
-        }
-    }
-    // TODO: Allow initialization with a String
-    // or StringLiteral.
-    
-    public struct AttestationResponse {
-        let attestation: Data
-        let keyID: Data
     }
     
     public static func verifyAttestation(
@@ -72,12 +68,21 @@ extension AppAttest {
     // TODO: Namespace this struct/
     // Assertion.ClientResponse
     // Assertion.Challenge.Response
-    public struct AssertionResponse {
+    
+    // Client response
+    public struct AssertionResponse: Codable {
         let assertion: Data
         let clientData: Data
         let challenge: Data
+        
+        public init(assertion: Data, clientData: Data, challenge: Data) {
+            self.assertion = assertion
+            self.clientData = clientData
+            self.challenge = challenge
+        }
     }
     
+    // Server result (to be stored in database)
     public struct AssertionResult {
         let counter: Int
     }
@@ -100,4 +105,22 @@ extension AppAttest {
         )
         return AssertionResult(counter: Int(assertion.authenticatorData.counter))
     }
+}
+
+extension AppAttest {
+    public struct AppID: Codable {
+        let teamID: String
+        let bundleID: String
+        
+        var description: String {
+            "\(teamID).\(bundleID)"
+        }
+        
+        public init(teamID: String, bundleID: String) {
+            self.teamID = teamID
+            self.bundleID = bundleID
+        }
+    }
+    // TODO: Allow initialization with a String
+    // or StringLiteral.
 }
